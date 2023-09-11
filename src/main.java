9c9fa29e799590d8e4d4ea0bec31adf85507c22b
djdw04 <djdw04@student.cis.edu.hk>
2023-09-11 21:02:47 +0800

    initial

--- src/main.java
+++ src/main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
-public class main {
-    public static void Main(String[] args) {
+public class Main {
+    public static void main(String[] args) {
         System.out.println("Enter your age: ");
         Scanner scanner = new Scanner(System.in);
         int age = scanner.nextInt();
