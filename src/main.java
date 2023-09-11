ce8020aee0ab2aff211a51af0aa0924b1495bb42
djdw04 <djdw04@student.cis.edu.hk>
2023-09-11 21:01:11 +0800

    initial

--- src/main.java
+++ src/main.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
 public class main {
+    public static void Main(String[] args) {
+        System.out.println("Enter your age: ");
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+        int dateAge = (age-2);
+        System.out.println(age + "-year olds should date somebody who is at least " + dateAge + "-years old.");
+
+    }
 }
