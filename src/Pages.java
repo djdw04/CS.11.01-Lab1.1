5650098c88f2180175dd82f4f5465dccc1487ab1
djdw04 <djdw04@student.cis.edu.hk>
2023-09-11 21:12:08 +0800

    initial

--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 public class Pages {
-
     public static void main(String[] args) {
+        System.out.println("Enter your age: ");
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+        int bookValue = (100-age);
+        System.out.println(age + "-year olds should read at least " + bookValue + " pages before giving up on a book.");
 
     }
 }
