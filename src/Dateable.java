582376810e7184bd56ec491a8852dcf66caa401a
djdw04 <djdw04@student.cis.edu.hk>
2023-09-12 08:15:35 +0800

    initial

--- src/Dateable.java
+++ src/Dateable.java
@@ -4,7 +4,7 @@ public class Dateable {
         System.out.println("Enter your age: ");
         Scanner scanner = new Scanner(System.in);
         int age = scanner.nextInt();
-        int dateAge = (age-2);
+        int dateAge = ((age/2)+7);
         System.out.println(age + "-year olds should date somebody who is at least " + dateAge + "-years old.");
 
     }
